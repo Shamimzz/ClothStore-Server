7aac7040566bfd9135192724226fb39d811b4906
Shamim Islam <86652659+Shamimzz@users.noreply.github.com>
2021-11-27 01:26:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 5000;
 
-// for .env variable must have declare.
+// for .env variable must have declare..
 require('dotenv').config()
 
 // middlewaire................
